545afeb72f12f3c11f30c1ecc25998ae63f8cd8a
David Powell <david@drp.id.au>
2019-09-24 23:07:30 +1000

    set default route to queue

--- app/src/router.ts
+++ app/src/router.ts
@@ -21,6 +21,6 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
-    { path: '*', redirect: '/about' }
+    { path: '*', redirect: '/queue' }
   ]
 })
